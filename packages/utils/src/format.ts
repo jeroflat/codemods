8b7bff5e56e7747f21d40bb19d4e98310a965bd3
jeroflat <jeronimo.velasquez@glovoapp.com>
2022-08-01 00:16:18 +0200

    fix(utils~format): allow read prettier's config from root

--- packages/utils/src/format.ts
+++ packages/utils/src/format.ts
@@ -1,3 +1,32 @@
-import prettier from 'prettier';
+import * as prettier from 'prettier';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 
-export const format = (str: string) => prettier.format(str);
+const root = path.resolve(__dirname, '../../../');
+const prettierrcJson = '.prettierrc.json';
+
+type PrettierConfig = Record<string, string | boolean | number>;
+
+let defaultPrettierOpts: PrettierConfig = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+};
+
+let prettierConfig: PrettierConfig;
+
+try {
+  const prettierrc = fs.readFileSync(`${root}/${prettierrcJson}`, 'utf-8');
+
+  prettierConfig = JSON.parse(prettierrc);
+} catch (err) {
+  prettierConfig = defaultPrettierOpts;
+}
+
+export const format = (str: string) =>
+  prettier.format(str, {
+    ...prettierConfig,
+    parser: 'babel-ts',
+  });
