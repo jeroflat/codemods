1dc0819158205a20613056815c7b14ce99d858a3
jeroflat <jeronimo.velasquez@glovoapp.com>
2022-08-03 18:41:54 +0200

    refactor(packages): remove 'mods-cjs'

--- packages/mods-cjs/src/callExpresion.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (file, api) {
-  const j = api.jscodeshift;
-
-  const ast = j.callExpression(j.identifier('foo'), [j.identifier('bar')]);
-
-  return j(ast).toSource();
-};
